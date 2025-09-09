abdbb6c5046397ccaa2a4325ccbe6a7107226716
Carlos Zanella <cefzanella@gmail.com>
2025-09-09 12:35:18 -0300

    feat: add useWindowSize hook

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export function useWindowSize() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const onResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', onResize);
+    onResize();
+
+    return () => { window.removeEventListener('resize', onResize); };
+  }, []);
+
+  return [width, height];
+}
