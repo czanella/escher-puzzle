30570c7c1f1f2cf8fd99f97bf8365cd8411131d8
Carlos Zanella <cefzanella@gmail.com>
2025-10-02 09:56:29 -0300

    feat: add Infinite Photos option to Storybook

--- src/components/infinite-canvas/infinite-canvas.stories.tsx
+++ src/components/infinite-canvas/infinite-canvas.stories.tsx
@@ -7,7 +7,27 @@ teapotImage.src = teapot.src;
 teapotImage.decode();
 
 const drawMapping: Record<string, DrawFunction> = {
-  'Single Photo': ({ context }) => { context.drawImage(teapotImage, 0, 0) },
+  'Single Photo': ({ context }) => {
+    context.drawImage(teapotImage, 0, 0);
+  },
+  'Infinite Photos': ({ context, minX, minY, maxX, maxY }) => {
+    const w = maxX - minX;
+    const h = maxY - minY;
+    const startX = Math.floor(minX / teapotImage.width);
+    const startY = Math.floor(minY / teapotImage.height);
+    const countX = Math.ceil(w / teapotImage.width) + 1;
+    const countY = Math.ceil(h / teapotImage.height) + 1;
+
+    for (let i = 0; i < countY; ++i) {
+      for (let j = 0; j < countX; ++j) {
+        context.drawImage(
+          teapotImage,
+          (startX + j)*teapotImage.width,
+          (startY + i)*teapotImage.height,
+        );
+      }
+    }
+  },
 };
 
 const meta: Meta<typeof InfiniteCanvasComponent> = {
